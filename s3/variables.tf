f034b811df1d15d228c12237fe8e2bbf09535015
Sumati A Gadagi <134225744+swatigadagi@users.noreply.github.com>
2026-05-27 18:49:21 +0530

    Update variables.tf

--- s3/variables.tf
+++ s3/variables.tf
@@ -1,11 +1,9 @@
 variable "project" {
   type = string
 }
-
 variable "environment" {
   type = string
 }
-
 variable "cloudfront_distribution_arn" {
   description = "CloudFront Distribution ARN (optional)"
   type        = string
