b6816eeaf68164af41aa4e1ac1d9ff2be1d210d1
Sumati A Gadagi <134225744+swatigadagi@users.noreply.github.com>
2026-05-27 15:39:22 +0530

    Update nat.tf

--- vpc/nat.tf
+++ vpc/nat.tf
@@ -1,7 +1,6 @@
 resource "aws_eip" "nat" {
   count  = var.enable_nat_gateway ? length(var.azs) : 0
   domain = "vpc"
-
   tags = {
     Name        = "${var.project}-${var.environment}-nat-eip-${count.index}"
     Environment = var.environment
@@ -13,12 +12,10 @@ resource "aws_nat_gateway" "this" {
   count         = var.enable_nat_gateway ? length(var.azs) : 0
   allocation_id = aws_eip.nat[count.index].id
   subnet_id     = aws_subnet.public[count.index].id
-
   tags = {
     Name        = "${var.project}-${var.environment}-nat-${count.index}"
     Environment = var.environment
     Project     = var.project
   }
-
   depends_on = [aws_internet_gateway.this]
 }
