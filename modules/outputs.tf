fbe7c87683890e9ebe1c412df88dab4a93971998
Sumati A Gadagi <134225744+swatigadagi@users.noreply.github.com>
2026-05-27 14:22:53 +0530

    Add files via upload

---
+++ modules/outputs.tf
@@ -0,0 +1,23 @@
+output "vpc_id" {
+  value = aws_vpc.this.id
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_app_subnet_ids" {
+  value = aws_subnet.private_app[*].id
+}
+
+output "private_worker_subnet_ids" {
+  value = aws_subnet.private_worker[*].id
+}
+
+output "endpoint_security_group_ids" {
+  value = aws_security_group.endpoints[*].id
+}
+output "default_security_group_id" {
+  value       = aws_security_group.endpoints.id
+  description = "The security group ID for the VPC endpoints"
+}
---
+++ modules/security-groups.tf
@@ -0,0 +1,48 @@
+resource "aws_security_group" "endpoints" {
+  name        = "${var.project}-${var.environment}-endpoint-sg"
+  description = "Allow VPC resources to access interface endpoints"
+  vpc_id      = aws_vpc.this.id
+
+  ingress {
+    description = "HTTPS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
+  }
+  ingress {
+    description = "HTTPS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
+  }
+  ingress {
+    description = "HTTPS from VPC"
+    from_port   = 8097
+    to_port     = 8097
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
+  }
+  ingress {
+    description = "HTTPS from VPC"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
+  }
+
+  egress {
+    description = "Allow all outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "${var.project}-${var.environment}-endpoint-sg"
+    Environment = var.environment
+    Project     = var.project
+  }
+}
