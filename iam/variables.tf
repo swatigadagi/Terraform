dfcc857cd4eaf309b83b84504f8b69fb92295f25
Sumati A Gadagi <134225744+swatigadagi@users.noreply.github.com>
2026-05-27 15:31:38 +0530

    Update variables.tf

--- iam/variables.tf
+++ iam/variables.tf
@@ -1,11 +1,9 @@
 variable "project" {
   type = string
 }
-
 variable "environment" {
   type = string
 }
-
 variable "ecr_repository_arn" {
   description = "ECR repository ARN for backend"
   type        = string
