9d097f0e658d61a3ff74eef018ff6ffed0e0cc94
Sumati A Gadagi <134225744+swatigadagi@users.noreply.github.com>
2026-05-27 21:49:23 +0530

    Update main.tf

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket  = "backend-workers-node"
     key     = "terraform/terraform.tfstate"
-    region  = "us-east-1"
+    region  = "ap-southeast-2"
     encrypt = true
   }
 }
