b3fca2a7bd1385095ad2b299d8d16f6a6a691d3e
Sumati A Gadagi <134225744+swatigadagi@users.noreply.github.com>
2026-05-27 15:15:29 +0530

    Update main.tf

--- ecr/main.tf
+++ ecr/main.tf
@@ -1,12 +1,9 @@
 resource "aws_ecr_repository" "this" {
   name = lower(trim("${var.project}-${var.environment}", "-"))
-
   image_tag_mutability = "MUTABLE"
-
   image_scanning_configuration {
     scan_on_push = true
   }
-
   tags = {
     Project     = var.project
     Environment = var.environment
