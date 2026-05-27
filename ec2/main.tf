175d4c9d8950c4fff39333573780cc278816e419
Sumati A Gadagi <134225744+swatigadagi@users.noreply.github.com>
2026-05-27 18:34:37 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,24 +1,5 @@
-################################################
-# Ubuntu AMI (Latest)
-################################################
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["099720109477"] # Canonical
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
-################################################
 # EC2 Security Group
-################################################
 resource "aws_security_group" "ec2" {
   name        = "${var.project}-${var.environment}-ec2-sg"
   description = "EC2 access SG for SSH only"
@@ -31,22 +12,18 @@ resource "aws_security_group" "ec2" {
     protocol    = "tcp"
     cidr_blocks = var.allowed_ssh_cidr
   }
-
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   tags = {
     Name = "${var.project}-${var.environment}-ec2-sg"
   }
 }
 
-################################################
 # IAM Role for EC2
-################################################
 resource "aws_iam_role" "ec2_role" {
   name = "${var.project}-${var.environment}-ec2-role"
 
@@ -72,22 +49,31 @@ resource "aws_iam_instance_profile" "ec2_profile" {
   role = aws_iam_role.ec2_role.name
 }
 
-################################################
+# Latest Ubuntu AMI
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 # EC2 Instance
-################################################
 resource "aws_instance" "ec2" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
-  
-  subnet_id              = var.public_subnet_id
+  subnet_id = var.public_subnet_id
   vpc_security_group_ids = [
-  aws_security_group.ec2.id,
-  var.rds_security_group_id
-   ]
+    aws_security_group.ec2.id,
+    var.rds_security_group_id
+  ]
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
-
   associate_public_ip_address = true
-
   tags = {
     Name = "${var.project}-${var.environment}-ec2"
   }
