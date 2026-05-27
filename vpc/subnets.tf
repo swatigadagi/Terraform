a738b711740689a89e1953f91a83c7dbcb2932b4
Sumati A Gadagi <134225744+swatigadagi@users.noreply.github.com>
2026-05-27 15:41:56 +0530

    Update subnets.tf

--- vpc/subnets.tf
+++ vpc/subnets.tf
@@ -4,7 +4,6 @@ resource "aws_subnet" "public" {
   cidr_block              = var.public_subnets[count.index]
   availability_zone       = var.azs[count.index]
   map_public_ip_on_launch = true
-
   tags = {
     Name        = "${var.project}-${var.environment}-public-${count.index}"
     Tier        = "public"
@@ -17,22 +16,9 @@ resource "aws_subnet" "private_app" {
   vpc_id            = aws_vpc.this.id
   cidr_block        = var.private_app_subnets[count.index]
   availability_zone = var.azs[count.index]
-
   tags = {
     Name        = "${var.project}-${var.environment}-app-${count.index}"
     Tier        = "app"
-    Environment = var.environment
-  }
-}
-resource "aws_subnet" "private_worker" {
-  count             = length(var.private_worker_subnets)
-  vpc_id            = aws_vpc.this.id
-  cidr_block        = var.private_worker_subnets[count.index]
-  availability_zone = var.azs[count.index]
-
-  tags = {
-    Name        = "${var.project}-${var.environment}-worker-${count.index}"
-    Tier        = "worker"
     Environment = var.environment
   }
 }
