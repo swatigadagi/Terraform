6d302e0b5dc2e711896dc053ba5af71e2961d48e
Sumati A Gadagi <134225744+swatigadagi@users.noreply.github.com>
2026-05-27 15:06:02 +0530

    Update variables.tf

--- environments/prod/variables.tf
+++ environments/prod/variables.tf
@@ -1,115 +1,59 @@
-
-
 variable "region" {
-  description = "AWS region"
+  description = "aws region"
   type        = string
-  default     = ""
-}
-
-variable "aws_account_id" {
-  description = "AWS account ID"
-  type        = string
-}
-
-variable "AWS_REGION" {
-  description = "AWS region"
-  type        = string
-  default     = ""
 }
 
 variable "project" {
-  description = "Project name"
+  description = "project name"
   type        = string
 }
 
 variable "environment" {
-  description = "Environment name"
+  description = "env name"
   type        = string
 }
 
-variable "artifact_bucket" {
-  description = "S3 bucket used by CodePipeline for artifacts"
+variable "certificate_arn" {
+  description = "acm cert arn"
   type        = string
 }
 
-variable "frontend_build_name" {
-  description = "CodeBuild project name for frontend"
-  type        = string
-}
-
-variable "backend_build_name" {
-  description = "CodeBuild project name for backend"
-  type        = string
-}
-
-variable "worker_build_name" {
-  description = "CodeBuild project name for worker"
-  type        = string
-}
-
-variable "bitbucket_repo" {
-  description = "Bitbucket repository name"
-  type        = string
-}
-
-variable "bitbucket_branch" {
-  description = "Bitbucket branch used by pipeline"
-  type        = string
-}
-
-
-variable "container_port"{
-  description = "Host Port"
+variable "container_port" {
+  description = "container port"
   type        = number
 }
 
-
-variable "master_password" {
-  type      = string
-  sensitive = true
-}
-
-variable "name" {
-  type    = string
-  default = "seaverse"
-}
-
-variable "master_username" {
-  type    = string
-  default = "seaverse_admin"
-}
-
-variable "min_capacity" {
-  type    = number
-  }
-
-variable "max_capacity" {
-  type    = number
-}
-
-
-variable "visibility_timeout" {
-  type = number
-}
-
-variable "message_retention" {
-  type = number
-}
-
-variable "max_receive_count" {
-  type = number
-}
-
-
-variable "domain_name" {}
-variable "frontend_subdomain" {}
-variable "backend_subdomain" {}
-
-
-
 variable "allowed_ssh_cidr" {
-  description = "CIDR blocks allowed to SSH into EC2"
+  description = "ssh access cidr"
   type        = list(string)
 
   default = ["0.0.0.0/0"]
+}
+
+variable "name" {
+  description = "db name"
+  type        = string
+  default     = "seaverse"
+}
+
+variable "master_username" {
+  description = "db master username"
+  type        = string
+  default     = "seaverse_admin"
+}
+
+variable "master_password" {
+  description = "db password"
+  type        = string
+  sensitive   = true
+}
+
+variable "min_capacity" {
+  description = "aurora min capacity"
+  type        = number
+}
+
+variable "max_capacity" {
+  description = "aurora max capacity"
+  type        = number
 }
