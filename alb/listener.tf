f6b29fac6092d95d66a82f6fae626303202d3afd
Sumati A Gadagi <134225744+swatigadagi@users.noreply.github.com>
2026-05-28 12:58:07 +0530

    Update listener.tf

--- alb/listener.tf
+++ alb/listener.tf
@@ -1,3 +1,16 @@
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+
+  certificate_arn = aws_acm_certificate_validation.this.certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.backend.arn
+  }
+}
 #resource "aws_lb_listener" "https" {
 #  load_balancer_arn = aws_lb.this.arn
 #  port              = 443
