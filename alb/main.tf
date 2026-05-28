c1aa2625812d67e94a433bbd6ee9d8c9fdd7bbec
Sumati A Gadagi <134225744+swatigadagi@users.noreply.github.com>
2026-05-28 13:40:36 +0530

    Update main.tf

--- alb/main.tf
+++ alb/main.tf
@@ -3,6 +3,7 @@ resource "aws_lb" "this" {
   load_balancer_type = "application"
   subnets            = var.public_subnet_ids
   security_groups    = [aws_security_group.alb.id]
+  route53_zone_id = var.route53_zone_id
   listener_arn = module.alb.listener_arn
   tags = {
     Project     = var.project
