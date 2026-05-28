3887d4a92f2342420a1045dedd5b3319ccc41cba
Sumati A Gadagi <134225744+swatigadagi@users.noreply.github.com>
2026-05-28 15:40:02 +0530

    Update outputs.tf

--- rds/outputs.tf
+++ rds/outputs.tf
@@ -1,24 +1,3 @@
-output "cluster_endpoint" {
-  description = "Writer endpoint for the Aurora cluster"
-  value       = aws_rds_cluster.this.endpoint
-}
-
-output "cluster_reader_endpoint" {
-  description = "Reader endpoint for the Aurora cluster"
-  value       = aws_rds_cluster.this.reader_endpoint
-}
-
-output "cluster_id" {
-  value = aws_rds_cluster.this.id
-}
-
-output "db_name" {
-  value = aws_rds_cluster.this.database_name
-}
-
-output "port" {
-  value = aws_rds_cluster.this.port
-}
 output "rds_security_group_id" {
   value = aws_security_group.rds.id
 }
