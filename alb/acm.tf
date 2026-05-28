fdcb124c810e62d4483209e37785f9f6ddcbb39d
Sumati A Gadagi <134225744+swatigadagi@users.noreply.github.com>
2026-05-28 13:03:33 +0530

    Update acm.tf

--- alb/acm.tf
+++ alb/acm.tf
@@ -1,9 +1,11 @@
 resource "aws_acm_certificate" "this" {
-  domain_name       = "${var.project}.example.com"
+  domain_name       = var.domain_name
   validation_method = "DNS"
+
   lifecycle {
     create_before_destroy = true
   }
+
   tags = {
     Project     = var.project
     Environment = var.environment
