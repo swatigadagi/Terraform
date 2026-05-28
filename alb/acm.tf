dd7d8310f1bc88c4a214291e9af429024904e26f
Sumati A Gadagi <134225744+swatigadagi@users.noreply.github.com>
2026-05-28 13:39:50 +0530

    Update acm.tf

--- alb/acm.tf
+++ alb/acm.tf
@@ -1,15 +1,18 @@
-resource "aws_acm_certificate" "this" {
-  domain_name       = var.domain_name
-  validation_method = "DNS"
-
-  lifecycle {
-    create_before_destroy = true
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.this.domain_validation_options :
+    dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      value  = dvo.resource_record_value
+    }
   }
 
-  tags = {
-    Project     = var.project
-    Environment = var.environment
-  }
+  zone_id = var.route53_zone_id
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.value]
+  ttl     = 60
 }
 resource "aws_acm_certificate_validation" "this" {
   certificate_arn         = aws_acm_certificate.this.arn
