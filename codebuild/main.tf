b95a7e8aa92256e10ec74945ee2730a73a914514
Sumati A Gadagi <134225744+swatigadagi@users.noreply.github.com>
2026-05-28 10:43:46 +0530

    Update main.tf

--- codebuild/main.tf
+++ codebuild/main.tf
@@ -1,12 +1,26 @@
 provider "aws" {
   region = var.region
 }
-
 # CodeBuild IAM Role
 
-resource "aws_iam_role_policy_attachment" "codebuild_policy" {
-  role       = aws_iam_role.codebuild.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+resource "aws_iam_role" "codebuild" {
+  name = "codebuild-service-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "codebuild.amazonaws.com"
+        }
+
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
 }
 
 # Frontend
